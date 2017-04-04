d361ad3ef65ef613dcc2aa2e781dfd6374c2fdf8
Rasmus Fredrikson <rasmuf@kth.se>
2017-04-04 15:16:17 +0200

    panic

--- moviebase/app/js/battleCtrl.js
+++ moviebase/app/js/battleCtrl.js
@@ -1,18 +1,17 @@
 // Search controller that we use whenever we have a search inputs
 // and search results
 pokemonPlannerApp.controller('BattleCtrl', function ($scope,Pokemon, $firebaseArray) {
-	// TODO in Lab 5: you will need to implement a method that searchers for dishes
-	// including the case while the search is still running.
+    // TODO in Lab 5: you will need to implement a method that searchers for dishes
+    // including the case while the search is still running.
 
-	var ref = firebase.database().ref().child("players/"+Pokemon.getPlayer().toString());
+    var refMyPokemon = firebase.database().ref().child("players/" + Pokemon.getPlayer().toString());
+    $scope.otherPlayer = Pokemon.getPlayer() == 1 ? 2:1;
 
-  $scope.otherPlayer = Pokemon.getPlayer() == 1 ? 2:1;
-  	// create a synchronized array
+    var refOpponentPokemon = firebase.database().ref().child("players/" + $scope.otherPlayer);
+
+
+    // create a synchronized array
     // click on `index.html` above to see it used in the DOM!
-  	$scope.myPokemon = $firebaseArray(ref);
-  	$scope.myPokemonData = $scope.myPokemon.$loaded(function() {
-  		$scope.myPokemonData = $scope.myPokemon.$getRecord($scope.myPokemon.$keyAt(0)); 
-  	},function(error) {
-  		console.log(error);
-  	});
+    $scope.myPokemon = $firebaseArray(refMyPokemon);
+    $scope.opponentPokemon = $firebaseArray(refOpponentPokemon)
 });
