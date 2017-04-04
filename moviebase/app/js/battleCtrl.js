ec8e56bf5f305e6957b5261b86ada20ca2da7b2a
Sofia Ödegaard Jacobsson <Sofia degaard Jacobsson>
2017-04-04 16:12:41 +0200

    Added chosenPokemon to Battle

--- moviebase/app/js/battleCtrl.js
+++ moviebase/app/js/battleCtrl.js
@@ -1,13 +1,13 @@
 // Search controller that we use whenever we have a search inputs
 // and search results
-pokemonPlannerApp.controller('BattleCtrl', function ($scope,Pokemon, $firebaseArray) {
+pokemonPlannerApp.controller('BattleCtrl', function ($scope,Pokemon,$firebaseArray) {
     // TODO in Lab 5: you will need to implement a method that searchers for dishes
     // including the case while the search is still running.
 
-    var refMyPokemon = firebase.database().ref().child("players/" + Pokemon.getPlayer().toString());
+    var refMyPokemon = firebase.database().ref().child("players/" + Pokemon.getPlayer().toString() + "/chosenPokemon");
     $scope.otherPlayer = Pokemon.getPlayer() == 1 ? 2:1;
 
-    var refOpponentPokemon = firebase.database().ref().child("players/" + $scope.otherPlayer);
+    var refOpponentPokemon = firebase.database().ref().child("players/" + $scope.otherPlayer + "/chosenPokemon");
 
 
     // create a synchronized array
