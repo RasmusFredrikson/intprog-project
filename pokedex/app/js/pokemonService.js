6f49d6ae2bbfa96f13287f630992b6f1a48f6fb4
Rasmus Fredrikson <rasmuf@kth.se>
2017-04-05 14:13:03 +0200

    Renamed moviebase to pokedex

---
+++ pokedex/app/js/pokemonService.js
@@ -0,0 +1,125 @@
+// Here we create an Angular service that we will use for our 
+// model. In your controllers (or other services) you can include the
+// dependency on any service you need. Angular will insure that the
+// service is created first time it is needed and then just reuse it
+// the next time.
+pokemonPlannerApp.factory('Pokemon',function ($resource, $timeout) {
+
+    // Set the configuration for your app
+    // TODO: Replace with your project's config object
+    var player = 1;
+
+    var config = {
+        apiKey: "AIzaSyCopvBmpePv8mlx529uPfA2YZ9DJWve5qA",
+        authDomain: "pokedex-baee2.firebaseapp.com",
+        databaseURL: "https://pokedex-baee2.firebaseio.com/",
+        storageBucket: "pokedex-baee2.appspot.com"
+    };
+    firebase.initializeApp(config);
+
+    // Get a reference to the database service
+    var database = firebase.database();
+
+    this.setPlayer = function(newPlayer){
+        player = newPlayer;
+    }
+
+    this.getPlayer = function(){
+        return player;
+    }
+
+    this.addPokemon = function(){
+        var ref = database.ref("players")
+        ref.child(1).set({
+            id: "p1"
+            /*name: pokemon.name,
+            height: pokemon.height,
+            weight: pokemon.weight
+            */
+        });
+        ref.child(2).set({
+            id: "p2"
+            /*name: pokemon.name,
+            height: pokemon.height,
+            weight: pokemon.weight
+            */
+        });
+    }
+
+    this.addUserPokemon = function(player, pokemon){
+        var ref = database.ref("players/" + player)
+        ref.child(pokemon.national_id).set({
+            id: pokemon.national_id,
+            name: pokemon.name,
+            height: pokemon.height,
+            weight: pokemon.weight
+        })
+    }
+
+    // Get all pokémon in the database
+    this.getAllUserPokemon = function(cb) {
+        var list = [];
+        var ref = database.ref("pokemon");
+        ref.once('value').then(function(snapshot) {
+            //console.log(snapshot.val());
+            list.push(snapshot.val());
+        });
+        return list;
+    }
+
+    // Get a pokémon with a specified id
+    this.getPokemon = function(id) {
+        var ref = database.ref("pokemon/" + id);
+        ref.once('value').then(function(snapshot) {
+            console.log(snapshot.val());
+            return snapshot.val();
+        });
+    }
+
+    this.search = function() {
+        var ref = database.ref("pokemon");
+        ref.orderByValue().on("value", function(snapshot) {
+            console.log(snapshot.val());
+        });
+    }
+
+    this.PokemonSearch = $resource('http://pokeapi.co/api/v1/pokemon/:id/',{},{
+        get: {
+            headers: 
+            {
+                'Accept': 'application/json'
+            }
+        }
+    });
+
+    this.getDescription = $resource('http://pokeapi.co/:link',{},{
+        get: {
+            headers: 
+            {
+                'Accept': 'application/json'
+            }
+        }
+    });
+
+
+
+
+
+
+// TODO in Lab 5: Add your model code from previous labs
+// feel free to remove above example code
+// you will need to modify the model (getDish and getAllDishes) 
+// a bit to take the advantage of Angular resource service
+// check lab 5 instructions for details
+
+
+
+
+
+// Angular service needs to return an object that has all the
+// methods created in it. You can consider that this is instead
+// of calling var model = new DinnerModel() we did in the previous labs
+// This is because Angular takes care of creating it when needed.
+return this;
+
+});
---
+++ pokedex/app/js/sidebarCtrl.js
@@ -0,0 +1,37 @@
+// Movie controller that we use whenever we have view that needs to 
+// display or modify the Movie menu
+pokemonPlannerApp.controller('SidebarCtrl', function ($scope,Pokemon, $firebaseObject) {
+
+	// TODO in Lab 5: Implement the methods to get the Movie menu
+	// add dish to menu and get total menu price
+	$scope.search = function() {
+		$scope.status = "Searching...";
+		Pokemon.search();
+		/*function(data){
+			$scope.pokemon=data.results;
+			$scope.status = "Showing " + data.results.length + " results";
+		},function(data){
+			$scope.status = "There was an error";
+		});*/
+	}
+
+	$scope.getAllPokemon = function() {
+		Pokemon.getAllPokemon();
+	}
+
+	$scope.getPokemon = function(id) {
+		Pokemon.getPokemon(id);
+	}
+
+	$scope.myPokemon = [
+	{
+		"name": "Bulbasaur",
+		"type": "grass"
+	},
+	{
+		"name": "Pikachu",
+		"type": "electric"
+	}];
+
+
+});
