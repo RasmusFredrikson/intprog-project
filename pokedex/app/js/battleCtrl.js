f2b159cf0096eb127afc0cb0b3455fe78db8a380
Rasmus Fredrikson <rasmuf@kth.se>
2017-04-13 23:33:53 +0200

    Added surrender functionality and more information exchange between the two players

--- pokedex/app/js/battleCtrl.js
+++ pokedex/app/js/battleCtrl.js
@@ -7,42 +7,42 @@ pokemonPlannerApp.controller('BattleCtrl', function ($scope,Pokemon,$firebaseObj
     var refMyPokemon = firebase.database().ref().child("players/" + Pokemon.getPlayer().toString() + "/chosenPokemon");
     $firebaseObject(refMyPokemon).$loaded().then(function() {
         $scope.myPokemon = $firebaseObject(refMyPokemon);
-            //console.log(typeof $scope.myPokemon.pokemon.hp);
-            $scope.data = [50];
-            //console.log($scope.data);
-            $scope.svg = d3.select("#myPokemonSVG");
-            //console.log($scope.svg);
-            $scope.g = $scope.svg.append("g");
-            $scope.g.selectAll(".bar")
-            .data([50])
-            .enter().append("rect")
-            .attr("class", "bar")
-            .attr("x", 0)
-            .attr("y", 0)
-            .attr("width", function(d) {
-                return d;
-            })
-            .attr("height", 10)
-            .style("fill", "red");
-        });
+    //console.log(typeof $scope.myPokemon.pokemon.hp);
+    $scope.data = [50];
+    //console.log($scope.data);
+    $scope.svg = d3.select("#myPokemonSVG");
+    //console.log($scope.svg);
+    $scope.g = $scope.svg.append("g");
+    $scope.g.selectAll(".bar")
+    .data([50])
+    .enter().append("rect")
+    .attr("class", "bar")
+    .attr("x", 0)
+    .attr("y", 0)
+    .attr("width", function(d) {
+        return d;
+    })
+    .attr("height", 10)
+    .style("fill", "red");
+});
 
     setTimeout(function(){
         $scope.g.selectAll(".bar").data([20]);
         d3.select("#myPokemonSVG").transition()
-        .selectAll(".bar")   // change the bars
-        .duration(1000)
-        .attr("x", 0)
-        .attr("y", 0)
-        .attr("width", function(d) {
-            return d;
-        })
-        .attr("height", 10)
-        .style("fill", "blue");
-    },5000);
+    .selectAll(".bar")   // change the bars
+    .duration(1000)
+    .attr("x", 0)
+    .attr("y", 0)
+    .attr("width", function(d) {
+        return d;
+    })
+    .attr("height", 10)
+    .style("fill", "blue");
+},5000);
 
-    
 
-    
+
+
     $scope.otherPlayer = Pokemon.getPlayer() == 1 ? 2:1;
     var refOpponentPokemon = firebase.database().ref().child("players/" + $scope.otherPlayer + "/chosenPokemon");
     $firebaseObject(refOpponentPokemon).$loaded().then(function() {
@@ -53,18 +53,24 @@ pokemonPlannerApp.controller('BattleCtrl', function ($scope,Pokemon,$firebaseObj
     var refTurn = firebase.database().ref().child("settings/turn");
     $scope.turn = $firebaseObject(refTurn);
 
+    var refStatus = firebase.database().ref().child("settings/status");
+    $scope.status = $firebaseObject(refStatus);
+
     $scope.playerAttacks = function(move) {
         console.log(move.length);
         $scope.opponentPokemon.pokemon.hp -= move.length;
-        $scope.status = $scope.myPokemon.pokemon.name + " caused " + move.length + " damage on " + $scope.opponentPokemon.pokemon.name + "!";
+        $scope.status.desc = $scope.myPokemon.pokemon.name + " caused " + move.length + " damage on " + $scope.opponentPokemon.pokemon.name + "!";
+        $scope.turn.player = $scope.otherPlayer;
+        console.log($scope.status.desc)
         if ($scope.opponentPokemon.pokemon.hp <= 0) {
             $scope.opponentPokemon.pokemon.hp = 0;
             $scope.opponentPokemon.$save().then(function() {
                 alert("Opponent lost!");
-            });            
+            });
+            $scope.turn.player = false;
         }
         $scope.opponentPokemon.$save();
-        $scope.turn.player = $scope.otherPlayer;
+        $scope.status.$save();
         $scope.turn.$save();
     }
 
@@ -88,20 +94,35 @@ pokemonPlannerApp.controller('BattleCtrl', function ($scope,Pokemon,$firebaseObj
     }
 
     $scope.reset = function() {
-      console.log("Running reset!");
+        console.log("Running reset!");
+        alert("You abandoned the game :(");
+        $scope.turn.player = false;
+        refOpponentPokemon.remove();
+        refMyPokemon.remove();
+        $scope.turn.$save();
     }
 
     $scope.opponentAttacks = function(move) {
         console.log(move);
         $scope.myPokemon.pokemon.hp -= move.length;
-        $scope.status = $scope.opponentPokemon.pokemon.name + " caused " + move.length + " damage on " + $scope.myPokemon.pokemon.name + "!";
+        $scope.status.desc = $scope.opponentPokemon.pokemon.name + " caused " + move.length + " damage on " + $scope.myPokemon.pokemon.name + "!";
         if ($scope.myPokemon.pokemon.hp <= 0) {
             $scope.myPokemon.pokemon.hp = 0;
             $scope.myPokemon.$save().then(function() {
                 alert("Player lost!");
             });
+            $scope.turn.player = false;
+            $scope.turn.$save();
         }
+        $scope.status.$save();
         $scope.myPokemon.$save();
     }
-    
+
+    $scope.checkStatus = function() {
+        if ($scope.opponentPokemon.pokemon == null)
+            return ("Waiting for player " + $scope.otherPlayer + "...");
+        else
+            return ("HP: " + $scope.opponentPokemon.pokemon.hp);
+    }
+
 });
